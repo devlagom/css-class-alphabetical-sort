ba9524b19f0e84f0d8115348db5e0f5bdd5e54fe
Oscar Riascos <osferibur@hotmail.com>
2024-10-30 09:17:55 -0500

    Fix: blade conditional classes

--- src/extension.ts
+++ src/extension.ts
@@ -1,37 +1,48 @@
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
 export function activate(context: vscode.ExtensionContext) {
-	const saveDisposable = vscode.workspace.onWillSaveTextDocument(event => {
-		const document = event.document;
-		const fileName = document.fileName;
+  const saveDisposable = vscode.workspace.onWillSaveTextDocument((event) => {
+    const document = event.document;
+    const fileName = document.fileName;
 
-		if (/\.(php|html|htm|svelte)$/.test(fileName)) {
-			const text = document.getText();
-			const sortedText = sortClassNames(text);
+    if (/\.(php|html|htm|svelte|js|jsx|ts|tsx)$/.test(fileName)) {
+      const text = document.getText();
+      const sortedText = sortClassNames(text);
 
-			const fullRange = new vscode.Range(
-				document.positionAt(0),
-				document.positionAt(text.length)
-			);
+      const fullRange = new vscode.Range(
+        document.positionAt(0),
+        document.positionAt(text.length)
+      );
 
-			event.waitUntil(Promise.resolve([
-				vscode.TextEdit.replace(fullRange, sortedText)
-			]));
-		}
-	});
+      event.waitUntil(
+        Promise.resolve([vscode.TextEdit.replace(fullRange, sortedText)])
+      );
+    }
+  });
 
-	context.subscriptions.push(saveDisposable);
+  context.subscriptions.push(saveDisposable);
 }
 
 function sortClassNames(text: string): string {
-	return text.replace(/class="([^"]+)"/g, (match, classList) => {
-		const sortedClassList = classList
-			.split(/\s+/)
-			.filter(Boolean)
-			.sort((a: string, b: string) => a.localeCompare(b))
-			.join(' ');
-		return `class="${sortedClassList}"`;
-	});
+  return text.replace(
+    /class(Name)?="([^"]+)"/g,
+    (match, classNameType, classList) => {
+      const bladeConditionMatch = classList.match(/{{[^}]+}}/);
+      const bladeCondition = bladeConditionMatch
+        ? bladeConditionMatch[0].trim()
+        : "";
+      const remainingClasses = classList.replace(bladeCondition, "").trim();
+
+      const sortedClassList = remainingClasses
+        .split(/\s+/)
+        .filter(Boolean)
+        .sort((a: string, b: string) => a.localeCompare(b))
+        .join(" ");
+
+      const outputClass = `${bladeCondition} ${sortedClassList}`;
+      return `class${classNameType || ""}="${outputClass.trim()}"`;
+    }
+  );
 }
 
-export function deactivate() { }
+export function deactivate() {}
